c81a7fbd0cd38f94a8021165cace8e0b0b8475ef
abdulaim1010 <alim.12016032@student.brur.ac.bd>
2026-09-02 00:30:31 +0600

    start to the blood donation

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Blood Donation Emergency Platform API is running",
+    data: null,
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
